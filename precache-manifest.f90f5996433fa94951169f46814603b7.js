67c102f377108512269ce65db6da6632a1f0ffaa
Jeremy Yeo <jeremy.yeo@xero.com>
2019-04-12 00:19:46 +1200

    Updates

--- precache-manifest.f90f5996433fa94951169f46814603b7.js
+++ precache-manifest.f90f5996433fa94951169f46814603b7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/where/static/js/runtime~main.854626a5.js"
   },
   {
-    "revision": "068d4222424421152b2f",
-    "url": "/where/static/js/main.0cc86e37.chunk.js"
+    "revision": "29d51fe50be3212f9ceb",
+    "url": "/where/static/js/main.32c55463.chunk.js"
   },
   {
     "revision": "29bd19578749e037f64c",
     "url": "/where/static/js/2.4a14e836.chunk.js"
   },
   {
-    "revision": "068d4222424421152b2f",
+    "revision": "29d51fe50be3212f9ceb",
     "url": "/where/static/css/main.f4414094.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/where/static/css/2.3ef50edd.chunk.css"
   },
   {
-    "revision": "6f1c1d4c0c792203c182a2f03b07832e",
+    "revision": "bbf19f559f8c71d201d8b3ffb9c4d104",
     "url": "/where/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/where/precache-manifest.1700340fcb9549eb85839a12074046e1.js"
+  "/where/precache-manifest.f90f5996433fa94951169f46814603b7.js"
 );
 
 workbox.clientsClaim();
